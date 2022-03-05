ae5e639b5bfd923445f7edda51d752b282d25d5c
Anders Aagaard Schütt <bluesheep100@hotmail.com>
2022-03-05 15:58:10 +0100

    Add logout button to navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,13 @@
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-dark bg-dark">
     <div class="container-fluid">
         <span class="navbar-brand mb-0 h1">Navbar</span>
+
+        <div class="ml-auto">
+            <form action="{{ route('logout') }}" method="post" class="form-inline">
+                @csrf
+
+                <button type="submit" class="btn btn-danger">Log out</button>
+            </form>
+        </div>
     </div>
 </nav>
