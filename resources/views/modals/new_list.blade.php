e10e9b464ce4507b38c48a5e4a2d0d7259f47a2d
Anders Aagaard Schütt <bluesheep100@hotmail.com>
2022-03-05 17:42:27 +0100

    Add 'New List' Modal

---
+++ resources/views/modals/new_list.blade.php
@@ -0,0 +1,26 @@
+<!-- Modal -->
+<div class="modal fade" id="new-list" tabindex="-1" aria-labelledby="new-list-label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="new-list-label">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route('todo-list.store') }}" method="post">
+                    @csrf
+
+                    <div class="mb-3">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+
+                    <div class="text-end">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/todo_list/index.blade.php
+++ resources/views/todo_list/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('modals')
+    @include('modals.new_list')
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row">
