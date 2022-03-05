33683c3eda7993006e179a503297f7595987939a
Anders Aagaard Schütt <bluesheep100@hotmail.com>
2022-03-05 16:15:55 +0100

    Define basic authorization gates

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Role::class);
     }
+
+    public function hasRole(string $roleName)
+    {
+        return $this->roles()->where('name', $roleName)->exists();
+    }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\TodoList;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +27,12 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('is-admin', function (User $user) {
+            return $user->hasRole('admin');
+        });
+
+        Gate::define('owns-list', function (User $user, TodoList $list) {
+            return $user->id === $list->user_id;
+        });
     }
 }
