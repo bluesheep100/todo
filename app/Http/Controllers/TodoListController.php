852d8410420ad773d9720214e07cfc11f69460b6
Anders Aagaard Schütt <bluesheep100@hotmail.com>
2022-03-05 16:37:03 +0100

    Add CRUD Controller methods and routes for TodoList

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -2,10 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TodoList;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+
 class TodoListController extends Controller
 {
     public function index()
     {
-        return view('todo_list.index');
+        $user = Auth::user();
+
+        return view('todo_list.index', [
+            'lists' => Gate::allows('is-admin') ?
+                TodoList::with('items')->get() :
+                $user->lists()->with('items')->get,
+        ]);
+    }
+
+    public function store()
+    {
+        TodoList::create(request()->validate(TodoList::RULES));
+
+        return to_route('home');
+    }
+
+    public function update(TodoList $list)
+    {
+        $list->update(request()->validate(TodoList::RULES));
+
+        return to_route('home');
+    }
+
+    public function destroy(TodoList $list)
+    {
+        $list->delete();
+
+        return to_route('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,15 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/lists', [TodoListController::class, 'index'])->name('home');
+    Route::group(['prefix' => '/lists'], function () {
+        Route::get('/', [TodoListController::class, 'index'])->name('home');
+        Route::post('/create', [TodoListController::class, 'store'])->name('todo-list.store');
+
+        Route::group(['prefix' => '/{list}'], function () {
+            Route::patch('/update', [TodoListController::class, 'update'])->name('todo-list.update');
+            Route::delete('/delete', [TodoListController::class, 'destroy'])->name('todo-list.destroy');
+        });
+    });
 });
 
 Route::permanentRedirect('/', '/lists');
