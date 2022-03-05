a29643321061d5ec589ec54d6f506b0e5cd16c63
Anders Aagaard Schütt <bluesheep100@hotmail.com>
2022-03-05 13:29:30 +0100

    Add TodoItem <-> TodoList relations

--- app/Models/TodoItem.php
+++ app/Models/TodoItem.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class TodoItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['todo_list_id', 'content', 'completed'];
+
+    public function list(): Relation
+    {
+        return $this->belongsTo(TodoList::class);
+    }
 }
