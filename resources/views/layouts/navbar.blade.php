fd7694393ae52c3438afa12786506b007cef9f1f
Anders Aagaard Schütt <bluesheep100@hotmail.com>
2022-03-05 17:41:50 +0100

    Add 'New List' button to navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,7 +3,12 @@
         <span class="navbar-brand mb-0 h1">To-Do</span>
 
         <div class="ml-auto">
-            <form action="{{ route('logout') }}" method="post" class="form-inline">
+            <button type="button" class="btn btn-primary d-inline-block" data-bs-toggle="modal" data-bs-target="#new-list">
+                New list
+                &nbsp;<i class="fas fa-plus"></i>
+            </button>
+
+            <form action="{{ route('logout') }}" method="post" class="d-inline-block">
                 @csrf
 
                 <button type="submit" class="btn btn-danger">Log out</button>
