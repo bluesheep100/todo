1a11fb9e33f6ad10fe0800526414b0a5e5545021
Anders Aagaard Schütt <bluesheep100@hotmail.com>
2022-03-05 13:50:52 +0100

    Add User roles and seeded admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\TodoItem;
 use App\Models\TodoList;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +20,10 @@ class DatabaseSeeder extends Seeder
         User::factory(10)->create();
         TodoList::factory(5)->create();
         TodoItem::factory(10)->create();
+        Role::factory()->create(['name' => 'admin']);
+
+        // Create an administrator
+        $user = User::factory()->create(['name' => 'admin', 'email' => 'admin@example.com']);
+        $user->roles()->sync(Role::where('name', 'admin')->first()->id);
     }
 }
